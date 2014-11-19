262b700a4f85067c3ae94b45806cdc38efb56ffa
Austin Byrd <austin.byrd@me.com>
2014-11-19 21:36:54 +0000

    Start returning stuff

--- app.rb
+++ app.rb
@@ -1,15 +1,16 @@
 require 'sinatra'
 
-get /v01/sheets/:sheet_id do
+get '/v01/sheets/:sheet_id' do
+  return "The sheet you requested is : " + params[:sheet_id].inspect
 end
 
-get /v01/sheets/:sheet_id/rows
+get '/v01/sheets/:sheet_id/rows' do
 end
 
-get /v01/sheets/:sheet_id/rows/:row_id
+get '/v01/sheets/:sheet_id/rows/:row_id' do
 end
 
-put /v01/sheets
+put '/v01/sheets' do
   # Create new sheet record and return sheet_id
 end
 
