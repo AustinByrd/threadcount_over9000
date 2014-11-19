152f60fa60bc1918c611459b2919fed5ae771fdc
Austin Byrd <austin.byrd@me.com>
2014-11-19 19:07:16 +0000

    First pass at ThreadCount_Over9000 app

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+get /v01/sheets/:sheet_id do
+end
+
+get /v01/sheets/:sheet_id/rows
+end
+
+get /v01/sheets/:sheet_id/rows/:row_id
+end
+
+put /v01/sheets
+  # Create new sheet record and return sheet_id
+end
+
+
